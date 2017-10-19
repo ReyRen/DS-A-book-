3cf73b3b6c8ff3f1394ccdd6a57f3f3808994b06
whoami <whoami@reyren179@gmail.com>
2017-10-19 19:25:51 -0400

    Initial Commit

---
+++ DS&A(book)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DS&A(book)
+//
+//  Created by Rey Ren on 2017/10/19.
+//  Copyright © 2017年 whoami. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
